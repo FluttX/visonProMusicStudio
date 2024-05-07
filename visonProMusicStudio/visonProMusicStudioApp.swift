992c29c370a6ec8811269687fda236d962b23ceb
alamin-karno <amin15-1951@diu.edu.bd>
2024-05-07 11:23:09 +0600

    Initial Commit

---
+++ visonProMusicStudio/visonProMusicStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  visonProMusicStudioApp.swift
+//  visonProMusicStudio
+//
+//  Created by Mac Mini - Rokomari on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct visonProMusicStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
